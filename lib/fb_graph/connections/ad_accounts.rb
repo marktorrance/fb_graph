3753c598e4fb9611fce9f9e9642589228c20a331
Mark Torrance <mtorrance@rocketfuel.com>
2012-11-06 13:12:24 -0800

    Add ad_campaign!

--- lib/fb_graph/connections/ad_accounts.rb
+++ lib/fb_graph/connections/ad_accounts.rb
@@ -9,6 +9,14 @@ module FbGraph
           )
         end
       end
+
+      def ad_account!(options = {})
+        ad_account = post options.merge(:connection => :adaccounts)
+        AdAccount.new ad_account[:id], options.merge(ad_account).merge(
+          :access_token => options[:access_token] || self.access_token
+        )
+      end
+
     end
   end
 end
